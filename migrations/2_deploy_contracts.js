b54b4c6700198f85dfd3bc661e17fc1b79e67f6f
Gustavo <gumastro@usp.br>
2022-02-14 02:18:03 -0300

    Feat: Transfer Catto to exchange
    Transfer all Catto ERC20 tokens to EthSwap contract to make them ready to be purchased

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 const CattoToken = artifacts.require("CattoToken");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-    deployer.deploy(CattoToken);
-    deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+    await deployer.deploy(CattoToken);
+    const catto = await CattoToken.deployed()
+
+    await deployer.deploy(EthSwap);
+    const ethswap = await EthSwap.deployed()
+
+    // Transfer all Catto tokens to EthSwap
+    await catto.transfer(ethswap.address, '1000000000000000000000000')
 };
