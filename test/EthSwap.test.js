da1cacc5f848b89a5af1cc29e5b7eeec674a4a04
Gustavo <gumastro@usp.br>
2022-02-15 19:24:46 -0300

    Test: Buy Catto from exchange

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const CattoToken = artifacts.require("CattoToken");
 const EthSwap = artifacts.require("EthSwap");
 
@@ -9,12 +11,12 @@ function tokens(n) {
     return web3.utils.toWei(n, 'ether');
 }
 
-contract('EthSwap', (accounts) => {
+contract('EthSwap', ([deployer, investor]) => {
     let cattoToken, ethSwap
 
     before(async() => {
         cattoToken = await CattoToken.new()
-        ethSwap = await EthSwap.new()
+        ethSwap = await EthSwap.new(cattoToken.address)
 
         await cattoToken.transfer(ethSwap.address, tokens('1000000'))
     })
@@ -35,6 +37,36 @@ contract('EthSwap', (accounts) => {
         it('contract has catto tokens', async() => {
             let balance = await cattoToken.balanceOf(ethSwap.address)
             assert.equal(balance.toString(), tokens('1000000'))
+        })
+    })
+
+    describe('buyCatto()', async() => {
+        let result;
+
+        before(async() => {
+            // Purchase Catto
+            result = await ethSwap.buyCatto({ from: investor, value: tokens('1') })
+        })
+
+        it('allows user to instantly purchase Catto from ethSwap for a fixed price', async() => {
+            // Check investor catto balance after purchase
+            let investorBalance = await cattoToken.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('100'))
+
+            // Check ethSwap catto balance after purchase
+            let ethSwapBalance = await cattoToken.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('999900'))
+
+            // Check ethSwap ether balance after purchase
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('1'))
+
+            // Check emited event after purchase
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, cattoToken.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
         })
     })
 })
