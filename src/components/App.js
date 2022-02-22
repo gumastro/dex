9f43075037901da29c3f85f1b4a85d42de2c5ac2
Gustavo <gumastro@usp.br>
2022-02-21 21:28:56 -0300

    Style: Navbar
    Display user wallet address and identicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,63 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import Navbar from './Navbar'
+import './App.css'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    // Get user MetaMask address
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    // Get user Ether balance
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethBalance: ethBalance })
+  }
+
+  // Connect to browser Wallet
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected.')
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      ethBalance: '0'
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
+                  href="#"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello world!</h1>
               </div>
             </main>
           </div>
