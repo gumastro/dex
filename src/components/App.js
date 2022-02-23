48362a5c87f88e5fddaa0a24439b94280404cf28
Gustavo <gumastro@usp.br>
2022-02-22 22:40:53 -0300

    Feat: Load token contracts to front-end
    Make it possible to show Ether/Catto current balance and buy Catto

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import CattoToken from '../abis/CattoToken.json'
+import EthSwap from '../abis/EthSwap.json'
 import Navbar from './Navbar'
+import Main from './Main'
 import './App.css'
 
 class App extends Component {
@@ -20,6 +23,30 @@ class App extends Component {
     // Get user Ether balance
     const ethBalance = await web3.eth.getBalance(this.state.account)
     this.setState({ ethBalance: ethBalance })
+
+    // Load CattoToken
+    const networkId = await web3.eth.net.getId()
+    const cattoData = CattoToken.networks[networkId]
+    if (cattoData) { 
+      const cattoToken = new web3.eth.Contract(CattoToken.abi, cattoData.address)
+      this.setState({ cattoToken : cattoToken })
+      let cattoBalance = await cattoToken.methods.balanceOf(this.state.account).call()
+      this.setState({ cattoBalance : cattoBalance.toString() })
+    } else {
+      window.alert('Token contract not deployed to detected network.')
+    }
+
+    // Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId]
+    if (ethSwapData) { 
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap : ethSwap })
+      console.log("ethSwap: ", ethSwap)
+    } else {
+      window.alert('EthSwap contract not deployed to detected network.')
+    }
+
+    this.setState({ loading : false })
   }
 
   // Connect to browser Wallet
@@ -36,28 +63,54 @@ class App extends Component {
     }
   }
 
+  buyCatto = (etherAmount) => {
+    console.log("buycatto called")
+    this.setState({ loading: true })
+    this.state.ethSwap.methods.buyCatto().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+      window.location.reload(true)
+    })
+  }
+
+
+
   constructor(props) {
     super(props)
     this.state = {
       account: '',
-      ethBalance: '0'
+      cattoToken: {},
+      ethSwap: {},
+      cattoBalance: '0',
+      ethBalance: '0',
+      loading: true
     }
   }
 
   render() {
+    let content
+
+    if (this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main
+        ethBalance={this.state.ethBalance}
+        cattoBalance={this.state.cattoBalance}
+        buyCatto={this.buyCatto}
+      />
+    }
     return (
       <div>
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px'}}>
               <div className="content mr-auto ml-auto">
                 <a
                   href="#"
                   rel="noopener noreferrer"
                 >
                 </a>
-                <h1>Hello world!</h1>
+                { content }
               </div>
             </main>
           </div>
